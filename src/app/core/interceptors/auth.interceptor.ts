6ebf7e2db9c193fd505db8f87c2c4e25a22570f5
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-13 20:31:18 +0300

    Created interceptor for handling login/register and logout responses from API.

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
+    HttpResponse
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { AuthService } from '../services';
+import { IUser } from '../models';
+
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(
+        private authService: AuthService
+    ) { }
+
+    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        return next
+            .handle(request)
+            .pipe(
+                tap(
+                    event => {
+                        if (event instanceof HttpResponse) {
+                            if (
+                                event.url?.endsWith('login') ||
+                                event.url?.endsWith('register')
+                            ) {
+                                this.authService.handleLogin(event.body as IUser);
+                                return;
+                            }
+
+                            if (
+                                event.url?.endsWith('logout')
+                            ) {
+                                this.authService.handleLogout();
+                                return;
+                            }
+                        }
+                    }
+                )
+            );
+    }
+}
