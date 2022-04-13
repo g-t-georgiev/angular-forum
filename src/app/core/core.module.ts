ef5133fa7b35d012a609d8a0f71992ca8cc96414
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-13 20:29:35 +0300

    Declared authentication service, attach-cookie interceptor and authentication interceptor as module providers.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,9 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { AuthService } from './services';
+import { AttachCookieInterceptor, AuthInterceptor } from './interceptors';
 
 
 @NgModule({
@@ -14,7 +17,19 @@ export class CoreModule {
     static forRoot(): ModuleWithProviders<CoreModule> {
         return {
             ngModule: CoreModule,
-            providers: []
+            providers: [
+                AuthService,
+                {
+                    provide: HTTP_INTERCEPTORS,
+                    useClass: AttachCookieInterceptor,
+                    multi: true
+                },
+                {
+                    provide: HTTP_INTERCEPTORS,
+                    useClass: AuthInterceptor,
+                    multi: true
+                }
+            ]
         }
     }
 }
