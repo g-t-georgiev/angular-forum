a4fef06796981f6b1367e37af6da65ad3f110430
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-19 01:01:24 +0300

    Extended service.
    Toggle state on initialization depending on preferences.
    Toggle state manually true switch control.
    Listen for changes of preferences and update state accordingly.

--- src/app/core/services/app-mode-switch.service.ts
+++ src/app/core/services/app-mode-switch.service.ts
@@ -1,70 +1,53 @@
-import { Injectable, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-const DARK = '(prefers-color-scheme: dark)';
-const LIGHT = '(prefers-color-scheme: light)';
 
-interface Mode {
-    name: 'dark' | 'light';
-    type: 'prefer' | 'default';
-}
 
-class ThemeMode implements Mode {
-
-    constructor(
-        public name: 'dark' | 'light', 
-        public type: 'prefer' | 'default'
-    ) {
-        this.name = name;
-        this.type = type;
-    }
-
-}
 
 @Injectable()
 export class AppModeSwitchService {
 
-    private _window = this.document.defaultView;
-    private _darkColorPreferenceQuery: MediaQueryList | undefined;
-    private _lightColorPreferenceQuery: MediaQueryList | undefined;
-    private _isDarkModePreferred: boolean = false;
-    private _isLightModePreferred: boolean = false;
-
-    private _themeMode: BehaviorSubject<Mode>;
-    themeMode$: Observable<Mode>;
+    private _isDarkModeOn: BehaviorSubject<boolean>;
+    isDarkModeOn$: Observable<boolean>;
 
     constructor(
         @Inject(DOCUMENT) private document: Document
     ) {
-        this._themeMode = new BehaviorSubject<Mode>(
-            this._isDarkModePreferred 
-            ? { name: 'dark', type: 'prefer' }
-            : this._isLightModePreferred 
-            ? { name: 'light', type: 'prefer' }
-            : { name: 'light', type: 'default' }
-        );
-        this.themeMode$ = this._themeMode.asObservable();
-
-        this._darkColorPreferenceQuery = this._window?.matchMedia?.(DARK);
-        this._lightColorPreferenceQuery = this._window?.matchMedia?.(LIGHT);
+        this._isDarkModeOn = new BehaviorSubject<boolean>(false);
+        this.isDarkModeOn$ = this._isDarkModeOn.asObservable();
     }
 
-    toggleThemeMode(ev: Event) { 
-        console.log(ev);
+    toggle(value: boolean) { 
+        this._isDarkModeOn.next(value);
+        this.document.body.classList.toggle('dark', value);
+        this.document.body.classList.toggle('light', !value);
     }
 
-    detectPreference(): void {
-        
-        if (this._darkColorPreferenceQuery?.matches) {
-            console.log('Dark mode preferred: ', this._darkColorPreferenceQuery?.matches);
-            this._isDarkModePreferred = true;
-        }
-
-        if (this._lightColorPreferenceQuery?.matches) {
-            console.log('Light mode preferred: ', this._lightColorPreferenceQuery?.matches);
-            this._isLightModePreferred = true;
-        }
-        
+    sync(): void {
+        const isDarkColorSchemePreferred = this.document.defaultView?.matchMedia('(prefers-color-scheme: dark)').matches ?? false;
+        this._isDarkModeOn.next(isDarkColorSchemePreferred);
+        this.document.body.classList.toggle('dark', isDarkColorSchemePreferred);
+        this.document.body.classList.toggle('light', !isDarkColorSchemePreferred);
     }
+
+    colorSchemePrefChangeHandler(ev: MediaQueryListEvent) {
+        this._isDarkModeOn.next(!ev.matches);
+        this.document.body.classList.toggle('dark', !ev.matches);
+        this.document.body.classList.toggle('light', ev.matches);
+    }
+
+    boundContextHandler = this.colorSchemePrefChangeHandler.bind(this);
+
+
+    subscribe() {
+        this.document.defaultView?.matchMedia('(prefers-color-scheme: dark)').addEventListener?.('change', this.boundContextHandler);
+        this.document.defaultView?.matchMedia('(prefers-color-scheme: light)').addEventListener?.('change', this.boundContextHandler);
+    }
+
+    unsubscribe() {
+        this.document.defaultView?.matchMedia('(prefers-color-scheme: dark)').removeEventListener?.('change', this.boundContextHandler);
+        this.document.defaultView?.matchMedia('(prefers-color-scheme: light)').removeEventListener?.('change', this.boundContextHandler);
+    }
+
 }
