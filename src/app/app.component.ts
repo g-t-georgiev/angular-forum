b55801ab2b11b1afb58884bfbacd93eded0439d7
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-19 01:04:28 +0300

    Listen for changes of color scheme preferensces.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AppModeSwitchService } from './core/services';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy { 
+    constructor(
+        private appModeService: AppModeSwitchService
+    ) { }
+
+    ngOnInit(): void {
+        this.appModeService.subscribe();
+    }
+
+    ngOnDestroy(): void {
+        this.appModeService.unsubscribe();
+    }
+    
+}
