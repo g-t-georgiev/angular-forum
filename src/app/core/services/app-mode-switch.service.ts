b3b8c71d719ce5e80e6903eec42b285272d925bf
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-18 20:43:46 +0300

    Extended app mode switch service.
    Added listener for prefer-color-scheme media query changes.
    Implemented method for detecting color-scheme-preference and adjust mode preference flags accordingly.

--- src/app/core/services/app-mode-switch.service.ts
+++ src/app/core/services/app-mode-switch.service.ts
@@ -1,10 +1,76 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, Inject, OnDestroy, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable()
-export class AppModeSwitchService {
+const DARK = 'prefers-color-scheme: dark';
+const LIGHT = 'prefers-color-scheme: light';
+
+interface Mode {
+    name: 'dark' | 'light';
+    type: 'prefer' | 'default';
+}
+
+class ThemeMode implements Mode {
 
     constructor(
-        @Inject(DOCUMENT) document: Document
-    ) { }
+        public name: 'dark' | 'light', 
+        public type: 'prefer' | 'default'
+    ) {
+        this.name = name;
+        this.type = type;
+    }
+
+}
+
+@Injectable()
+export class AppModeSwitchService implements OnInit, OnDestroy {
+
+    private _window = this.document.defaultView;
+    private _matchMedia = this._window?.matchMedia;
+    private _darkColorPreferenceQuery = this._matchMedia?.(DARK);
+    private _lightColorPreferenceQuery = this._matchMedia?.(LIGHT);
+    private _isDarkModePreferred: boolean = false;
+    private _isLightModePreferred: boolean = false;
+
+    private _themeMode: BehaviorSubject<Mode>;
+    themeMode$: Observable<Mode>;
+
+    constructor(
+        @Inject(DOCUMENT) private document: Document
+    ) {
+        this._themeMode = new BehaviorSubject<Mode>(
+            this._isDarkModePreferred 
+            ? { name: 'dark', type: 'prefer' }
+            : this._isLightModePreferred 
+            ? { name: 'light', type: 'prefer' }
+            : { name: 'light', type: 'default' }
+        );
+        this.themeMode$ = this._themeMode.asObservable();
+    }
+
+    ngOnInit(): void {
+        this._darkColorPreferenceQuery?.addEventListener('change', this.toggleThemeMode);
+        this._lightColorPreferenceQuery?.addEventListener('change', this.toggleThemeMode);
+    }
+
+    ngOnDestroy(): void {
+        this._darkColorPreferenceQuery?.removeEventListener('change', this.toggleThemeMode);
+        this._lightColorPreferenceQuery?.removeEventListener('change', this.toggleThemeMode);
+    }
+
+    toggleThemeMode(ev: Event) { 
+        console.log(ev);
+    }
+
+    detectPreference(): void {
+
+        if (this._darkColorPreferenceQuery?.matches) {
+            this._isDarkModePreferred = true;
+        }
+
+        if (this._lightColorPreferenceQuery?.matches) {
+            this._isLightModePreferred = true;
+        }
+        
+    }
 }
