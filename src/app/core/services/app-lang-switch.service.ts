270ca0c7cfbffb68bd5dbf038effc7bb755496e1
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-22 00:00:32 +0300

    Implemented current language toggling logic.

--- src/app/core/services/app-lang-switch.service.ts
+++ src/app/core/services/app-lang-switch.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+
+
+export const LangOptions = {
+    English: 'en',
+    Bulgarian: 'bg'
+}
 
 @Injectable()
 export class AppLangSwitchService {
 
+    private _currentLang = new BehaviorSubject<string>(LangOptions.English);
+    private _preferedLang = new BehaviorSubject<string>(LangOptions.English);
+
+    currentLang$: Observable<string> = this._currentLang.asObservable();
+    preferedLang$: Observable<string> = this._preferedLang.asObservable();
+
     constructor() { }
+
+    toggle(selectedLang: string): void {
+        this._currentLang.next(selectedLang);
+    }
+
 }
