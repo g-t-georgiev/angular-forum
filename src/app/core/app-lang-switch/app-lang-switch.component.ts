92f86da2e08020c77e681ff16ecc1938c0204515
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-22 00:00:05 +0300

    Implemented logic for toggling and listing current language.

--- src/app/core/app-lang-switch/app-lang-switch.component.ts
+++ src/app/core/app-lang-switch/app-lang-switch.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppLangSwitchService, LangOptions } from '../services';
+
+
 
 @Component({
     selector: 'app-app-lang-switch',
     templateUrl: './app-lang-switch.component.html',
     styleUrls: ['./app-lang-switch.component.css']
 })
-export class AppLangSwitchComponent implements OnInit {
+export class AppLangSwitchComponent {
 
-    constructor() { }
+    // Hard-coded list of supported languages
+    readonly langList = LangOptions;
+
+    currentLang$!: Observable<string>;
+    preferedLang$!: Observable<string>;
+
+    constructor(
+        private appLangSwitchService: AppLangSwitchService
+    ) { }
 
     ngOnInit(): void {
+        this.currentLang$ = this.appLangSwitchService.currentLang$;
+        this.preferedLang$ = this.appLangSwitchService.preferedLang$;
+    }
+
+    toggle(el: HTMLSelectElement): void {
+        this.appLangSwitchService.toggle(el.value);
     }
 
 }
