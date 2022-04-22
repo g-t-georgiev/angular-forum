89094a892b12241fff755e958bcd38500f9d024c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-22 23:40:15 +0300

    Initialized clear header notifications directive.

---
+++ src/app/core/directives/clear-notification.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ClearNotificationDirective } from './clear-notification.directive';
+
+describe('ClearNotificationDirective', () => {
+    it('should create an instance', () => {
+        const directive = new ClearNotificationDirective();
+        expect(directive).toBeTruthy();
+    });
+});
---
+++ src/app/core/directives/clear-notification.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, OnInit, OnDestroy, Input } from '@angular/core';
+import { MessageBus } from '../services';
+
+@Directive({
+    selector: '[clearNotification]'
+})
+export class ClearNotificationDirective implements OnInit, OnDestroy {
+
+    private timerId: any;
+    @Input('clearNotification') private clearInterval: number = 5e3;
+
+    constructor(
+        private messageBusService: MessageBus.MessageBusService
+    ) { }
+
+    ngOnInit(): void {
+        this.timerId = setTimeout(
+            () => {
+                this.messageBusService.clear();
+            },
+            this.clearInterval
+        );
+    }
+
+    ngOnDestroy(): void {
+        clearTimeout(this.timerId);
+    }
+
+}
