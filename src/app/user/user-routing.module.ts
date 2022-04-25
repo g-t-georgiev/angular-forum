08bc2c5793c9d47a3ef9506774ba2b5650a52457
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-25 13:51:29 +0300

    Added login/register paths loading sign component.

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SignComponent } from './sign/sign.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+        path: 'login',
+        component: SignComponent
+    },
+    {
+        path: 'register',
+        component: SignComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
