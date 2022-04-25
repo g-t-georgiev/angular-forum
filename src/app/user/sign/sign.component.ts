47e7f8a7228c3bcfb9def07eebd8abd2239ea09e
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-25 14:12:30 +0300

    Subscribed for URL changes.

--- src/app/user/sign/sign.component.ts
+++ src/app/user/sign/sign.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-sign',
     templateUrl: './sign.component.html',
     styleUrls: ['./sign.component.css']
 })
-export class SignComponent implements OnInit {
+export class SignComponent implements OnInit, OnDestroy {
+
+    private urlChange: Subscription | undefined;
+    currentUrl: string = '';
 
     constructor(
         private router: Router
     ) { }
 
     ngOnInit(): void {
+        this.urlChange = this.router
+            .events
+            .subscribe({
+                next: (ev: Event) => {
+                    if (ev instanceof NavigationStart) {
+                        this.currentUrl = 'Url change detected.'
+                    }
+
+                    if (ev instanceof NavigationEnd) {
+                        this.currentUrl = ev.url;
+                    }
+
+                    if (ev instanceof NavigationError) {
+                        this.currentUrl = 'Error occured during navigation.';
+                    }
+                }
+            })
+    }
+
+    ngOnDestroy(): void {
+        this.urlChange?.unsubscribe();
     }
 
 }
