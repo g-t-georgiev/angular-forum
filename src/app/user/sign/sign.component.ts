9970b548d27220a3dea8362fe4810cad7da7a903
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-25 14:00:10 +0300

    Injected router service.

--- src/app/user/sign/sign.component.ts
+++ src/app/user/sign/sign.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-sign',
-  templateUrl: './sign.component.html',
-  styleUrls: ['./sign.component.css']
+    selector: 'app-sign',
+    templateUrl: './sign.component.html',
+    styleUrls: ['./sign.component.css']
 })
 export class SignComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private router: Router
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
