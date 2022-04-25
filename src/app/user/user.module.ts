cf4e17578a35017dcf7b82e26a2cd686082c67b9
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-25 13:49:10 +0300

    Added sign component to declarations and exports.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
+import { SignComponent } from './sign/sign.component';
 
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        SignComponent
+    ],
     imports: [
         CommonModule,
         UserRoutingModule
+    ],
+    exports: [
+        SignComponent
     ]
 })
 export class UserModule { }
