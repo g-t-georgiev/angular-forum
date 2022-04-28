acb92eff40e1000544396705175a2d54f7c3a992
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-28 23:52:39 +0300

    Updated directive.
    Cancel timer if notification status is pinned.
    Take index input and pass it to the clearing method of message service.

--- src/app/core/directives/clear-notification.directive.ts
+++ src/app/core/directives/clear-notification.directive.ts
@@ -1,27 +1,42 @@
-import { Directive, OnInit, OnDestroy } from '@angular/core';
+import { Directive, Input, OnInit, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 import { MessageBus } from '../services';
 
 @Directive({
     selector: '[clearNotification]'
 })
-export class ClearNotificationDirective implements OnInit, OnDestroy {
+export class ClearNotificationDirective implements OnInit, OnChanges, OnDestroy {
+
 
     private timerId: any;
     private clearInterval: number;
+
+    @Input('clearNotification') cancelTimer: boolean;
+    @Input('notificationIndex') notificationIndex!: number;
 
     constructor(
         private messageBusService: MessageBus.MessageBusService
     ) {
         this.clearInterval = 5e3;
+        this.cancelTimer = false;
     }
 
     ngOnInit(): void {
         this.timerId = setTimeout(
             () => {
-                this.messageBusService.clear();
+                this.messageBusService.clear(this.notificationIndex);
             },
             this.clearInterval
         );
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        const { cancelTimer } = changes;
+        
+        if (cancelTimer?.currentValue) {
+            clearTimeout(this.timerId);
+            return;
+        }
+
     }
 
     ngOnDestroy(): void {
