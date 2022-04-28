2e08b2a3c7316ca258397268c6fd13b1cfb7549b
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-28 23:50:46 +0300

    Injected message service in testing environment.

--- src/app/core/directives/clear-notification.directive.spec.ts
+++ src/app/core/directives/clear-notification.directive.spec.ts
@@ -1,8 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
 import { ClearNotificationDirective } from './clear-notification.directive';
+import { MessageBus } from '../services';
 
 describe('ClearNotificationDirective', () => {
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [
+            MessageBus.MessageBusService
+        ]
+    }));
+
     it('should create an instance', () => {
-        const directive = new ClearNotificationDirective();
+        const messageService: MessageBus.MessageBusService = TestBed.inject(MessageBus.MessageBusService);
+        const directive: ClearNotificationDirective = new ClearNotificationDirective(messageService);
         expect(directive).toBeTruthy();
     });
 });
