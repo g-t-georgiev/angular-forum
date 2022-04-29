9823308920ed6b7894c13e1f718120adedc1f02e
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-29 19:20:48 +0300

    Updated declarations and exports.
    Added URL validator directive in declarations and exports.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClearNotificationDirective, ValidateEmailDirective } from './directives';
+import { ValidateUrlDirective } from './directives/validate-url.directive';
 
 @NgModule({
     declarations: [
         ClearNotificationDirective,
-        ValidateEmailDirective
+        ValidateEmailDirective,
+        ValidateUrlDirective
     ],
     imports: [
         CommonModule
     ],
     exports: [
         ClearNotificationDirective,
-        ValidateEmailDirective
+        ValidateEmailDirective,
+        ValidateUrlDirective
     ]
 })
 export class SharedModule { }
