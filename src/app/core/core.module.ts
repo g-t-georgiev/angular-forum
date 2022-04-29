7bcce54213ac05b5d1384d6221079ba3a6055113
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-29 18:51:33 +0300

    Refactored imports and declarations.
    Removed directives from declarations.
    Imported shared module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,30 +3,28 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { AuthService, MessageBus, AppThemeSwitchService, AppLangSwitchService } from './services';
 import { AttachCookieInterceptor, AuthInterceptor, ResponseMessageInterceptor } from './interceptors';
 import { HeaderComponent } from './header/header.component';
 import { AppThemeSwitchComponent } from './app-theme-switch/app-theme-switch.component';
 import { AppLangSwitchComponent } from './app-lang-switch/app-lang-switch.component';
-import { ClearNotificationDirective } from './directives/clear-notification.directive';
 
 
 @NgModule({
     declarations: [
         HeaderComponent,
         AppThemeSwitchComponent,
-        AppLangSwitchComponent,
-        ClearNotificationDirective
+        AppLangSwitchComponent
   ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        SharedModule
     ],
     exports: [
-        HeaderComponent,
-        // AppThemeSwitchComponent,
-        // AppLangSwitchComponent,
-        // ClearNotificationDirective
+        HeaderComponent
     ]
 })
 export class CoreModule {
