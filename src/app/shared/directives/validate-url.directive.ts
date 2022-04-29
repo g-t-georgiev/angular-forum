6de8b059d53c5f8e96898f2c39a6f951513682fa
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-04-29 19:20:15 +0300

    Instantiated URL validation directive.

---
+++ src/app/shared/directives/validate-url.directive.ts
@@ -0,0 +1,29 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+@Directive({
+    selector: '[appValidateUrl]',
+    providers: [
+        {
+            provide: NG_VALIDATORS,
+            useExisting: ValidateUrlDirective,
+            multi: true
+        }
+    ]
+})
+export class ValidateUrlDirective implements Validator {
+
+    validate(control: AbstractControl): ValidationErrors | null {
+        const { value } = control;
+
+        if (!value) return null;
+
+        return /(?=^https?:\/\/).+/.test(value)
+        ? null 
+        : {
+            invalidEmail: true
+        }
+
+    }
+
+}
