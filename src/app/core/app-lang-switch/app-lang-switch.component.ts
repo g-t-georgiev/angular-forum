07898e6ad1180c831bbd878e816610bac22bd386
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2022-05-04 03:19:24 +0300

    Refactored language switch component.

--- src/app/core/app-lang-switch/app-lang-switch.component.ts
+++ src/app/core/app-lang-switch/app-lang-switch.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AppLangSwitchService, AppThemeSwitchService, LangOptions } from '../services';
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { Subscription, combineLatest } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AppThemeSwitchService, LanguageService } from '../services';
 
 
 
@@ -9,26 +11,61 @@ import { AppLangSwitchService, AppThemeSwitchService, LangOptions } from '../ser
     templateUrl: './app-lang-switch.component.html',
     styleUrls: ['./app-lang-switch.component.css']
 })
-export class AppLangSwitchComponent {
+export class AppLangSwitchComponent implements OnInit, OnDestroy {
 
     // Hard-coded list of supported languages
-    readonly langList = LangOptions;
+    readonly languageOptions = LanguageService.LangOptions;
 
-    currentLang$!: Observable<string>;
-    isDarkModeOn$!: Observable<boolean>;
+    opened: boolean;
+    currentLang!: string;
+    darkModeOn!: boolean;
+    subscription!: Subscription;
 
     constructor(
-        private appLangSwitchService: AppLangSwitchService,
-        private appThemeSwitchService: AppThemeSwitchService
-    ) { }
-
-    ngOnInit(): void {
-        this.currentLang$ = this.appLangSwitchService.currentLang$;
-        this.isDarkModeOn$ = this.appThemeSwitchService.isDarkModeOn$;
+        private languageService: LanguageService.AppLangSwitchService,
+        private colorThemeService: AppThemeSwitchService,
+        @Inject(DOCUMENT) private document: Document
+    ) {
+        this.opened = false;
     }
 
-    toggle(el: HTMLSelectElement): void {
-        this.appLangSwitchService.toggle(el.value);
+    ngOnInit(): void {
+        this.subscription = combineLatest({
+            currentLang: this.languageService.currentLang$,
+            darkModeOn: this.colorThemeService.isDarkModeOn$
+        }).pipe(
+            tap(
+                ({ currentLang, darkModeOn }) => {
+                    this.currentLang = currentLang ?? 'N/A';
+                    this.darkModeOn = darkModeOn;
+                }
+            )
+        ).subscribe();
+    }
+
+    ngOnDestroy(): void {
+        this.subscription?.unsubscribe();
+    }
+
+    toggleDropdown(ev?: Event) {
+        this.opened = !this.opened;
+    }
+
+    toggleLanguage(ev: MouseEvent): void {
+        const selectedOption = ev.target as HTMLElement;
+        const selectedOptionValue = selectedOption.dataset['value'];
+        this.languageService.toggle(selectedOptionValue);
+    }
+
+    showSelectedLanguage(): string {
+        return Object
+            .entries(this.languageOptions)
+            .find(
+                ([ language_name, language_code ]) => {
+                    return language_code === this.currentLang;
+                }
+            )
+            ?.[0] ?? '';
     }
 
 }
